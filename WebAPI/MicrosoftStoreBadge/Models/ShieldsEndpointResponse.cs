26874a4747cc490c3d17d348ad15ab8b646fd74e
JasonWei512 <16899918+JasonWei512@users.noreply.github.com>
2023-04-13 19:43:06 +0800

    Web API: Replace DTO class with record

--- WebAPI/MicrosoftStoreBadge/Models/ShieldsEndpointResponse.cs
+++ WebAPI/MicrosoftStoreBadge/Models/ShieldsEndpointResponse.cs
@@ -2,24 +2,8 @@ using System.Text.Json.Serialization;
 
 namespace MicrosoftStoreBadge.Models;
 
-public class ShieldsEndpointResponse
+public record ShieldsEndpointResponse(string Label, string Message, Color Color = Color.BrightGreen, bool IsError = false, int SchemaVersion = 1)
 {
-    public string Label { get; set; }
-    public string Message { get; set; }
-
-    public Color Color { get; set; } = Color.Blue;
-    public bool IsError { get; set; } = false;
-
-    public int SchemaVersion { get; set; } = 1;
-
-    public ShieldsEndpointResponse(string label, string message, Color color = Color.BrightGreen, bool isError = false)
-    {
-        this.Label = label;
-        this.Message = message;
-        this.Color = color;
-        this.IsError = isError;
-    }
-
     public static ShieldsEndpointResponse Ok(string label, string message, Color color = Color.BrightGreen)
     {
         return new ShieldsEndpointResponse(label, message, color);
